eac93d85b7febceba1e8c0a00aa28ba4094a00eb
Tamer <62846154+Tamer3mansor@users.noreply.github.com>
2023-05-15 23:21:45 +0300

    init raw send email

--- controller/ketabk.js
+++ controller/ketabk.js
@@ -1,7 +1,15 @@
+let {sendEmail} = require('../utiles/mail');
+
 let signUp=((req,res)=>{
     let email=req.body.email;
     let password=req.body.password;
     let confirm=req.body.confirm;
+    console.log(sendEmail(email))
+    if(password)
+    {
+        console.log("here");
+        sendEmail(email)
+        }
     console.log(email,password,confirm);
     res.send(email,password,confirm)
 
